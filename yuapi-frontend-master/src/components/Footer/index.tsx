7d9ec13f6925533779825f981778ef0110b71f76
凉风 <2795176299@qq.com>
2024-08-12 17:57:10 +0800

    api接口平台前端项目

---
+++ yuapi-frontend-master/src/components/Footer/index.tsx
@@ -0,0 +1,36 @@
+import { GithubOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-components';
+import '@umijs/max';
+const Footer: React.FC = () => {
+  const defaultMessage = '凉风独家出品';
+  const currentYear = new Date().getFullYear();
+  return (
+    <DefaultFooter
+      style={{
+        background: 'none',
+      }}
+      copyright={`${currentYear} ${defaultMessage}`}
+      links={[
+        {
+          key: '凉风接口',
+          title: '凉风接口',
+          href: 'https://pro.ant.design',
+          blankTarget: true,
+        },
+        {
+          key: 'github',
+          title: <GithubOutlined />,
+          href: 'https://github.com/ant-design/ant-design-pro',
+          blankTarget: true,
+        },
+        {
+          key: '凉风接口',
+          title: '凉风接口',
+          href: 'https://ant.design',
+          blankTarget: true,
+        },
+      ]}
+    />
+  );
+};
+export default Footer;
